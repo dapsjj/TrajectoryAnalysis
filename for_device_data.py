d188da7d59fdc4ddc31e5b4f0df74c33ae703685
Rhaegal <dapsjj@qq.com>
2020-10-08 15:36:50 +0800

    Update for_device_data.py

--- for_device_data.py
+++ for_device_data.py
@@ -2,7 +2,6 @@ import pandas as pd
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import shutil
-
 import os
 import numpy as np
 from sklearn.cluster import DBSCAN
@@ -10,14 +9,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def testContectRemoteDatabase():
-    # df = pd.read_csv(r'E:/test_opencv/轨迹分析/device_data_20200924.csv', encoding='utf-8', parse_dates=[1], nrows=5)
-    # df = pd.read_csv(r'E:/test_opencv/轨迹分析/device_data_20200924.csv', encoding='utf-8', parse_dates=[1],  names=['device_id','upload_time','latitude','longitude','mileage','other_vals','speed'])
-    # df['upload_time_1'] = df['upload_time'].dt.strftime('%Y%m') #多了一列年月
-    # df.to_csv(r'E:/test_opencv/轨迹分析/device_data.csv', index=False, mode='w', header=True)
+    # 给csv增加一列年月
+    # df = pd.read_csv(r'E:/test_opencv/轨迹分析/device_data_20201007.csv', encoding='utf-8', parse_dates=[1], nrows=5)
+    df = pd.read_csv(r'E:/test_opencv/轨迹分析/device_data_20201007.csv', encoding='utf-8', parse_dates=[1],  names=['device_id','upload_time','latitude','longitude','mileage','other_vals','speed'])
+    df['upload_time_1'] = df['upload_time'].dt.strftime('%Y%m') #多了一列年月
+    df.to_csv(r'E:/test_opencv/轨迹分析/device_data.csv', index=False, mode='w', header=True)
     # latitude_list = df.latitude.values.tolist()
     # longitude_list = df.longitude.values.tolist()
 
-    '''
+
+
+    # 把大csv拆分成小csv
     df = pd.read_csv(r'E:/test_opencv/轨迹分析/device_data.csv', encoding='utf-8',parse_dates=[1],low_memory=False)
     #device_id长度[11,14,15,16]
     gb = df.groupby(['device_id', 'upload_time_1'])
@@ -49,25 +51,26 @@ def testContectRemoteDatabase():
         plt.savefig(r'E:/test_opencv/轨迹分析/all_device_data_image/'+str(device_id)+'_'+str(upload_time_1)+'.png')
         plt.close()
         i += 1
-    '''
 
-    '''
-    #把图片和csv复制到对应的文件夹
+
+
+
+    # 按照设备和人进行区别，把图片和csv复制到对应的文件夹
     imageDir = r'E:/test_opencv/轨迹分析/all_device_data_image/'
     csvDir = r'E:/test_opencv/轨迹分析/all_device_data_csv/'
-    person_img_dir = r'E:/test_opencv/轨迹分析/person_image'
-    device_img_dir = r'E:/test_opencv/轨迹分析/device_image'
-    person_csv_dir = r'E:/test_opencv/轨迹分析/person_csv'
-    device_csv_dir = r'E:/test_opencv/轨迹分析/device_csv'
+    person_img_dir = r'E:/test_opencv/轨迹分析/person_image/'
+    device_img_dir = r'E:/test_opencv/轨迹分析/device_image/'
+    person_csv_dir = r'E:/test_opencv/轨迹分析/person_csv/'
+    device_csv_dir = r'E:/test_opencv/轨迹分析/device_csv/'
 
     device_list = []
     person_list = []
     for i in os.listdir(imageDir):
         name = i.split('_')[0]
-        if len(name) == 11:
-            person_list.append(i.split('.')[0])
-        else: #长度14,15,16
+        if len(name) == 11 and name.startswith('17'): #车是17开头的，且长度是11位
             device_list.append(i.split('.')[0])
+        else: #长度14,15,16
+            person_list.append(i.split('.')[0])
 
     for item in person_list:
         imageName = imageDir + str(item) + '.png'
@@ -82,81 +85,100 @@ def testContectRemoteDatabase():
         if os.path.isfile(imageName) and os.path.isfile(csvlName):
             shutil.copy2(imageName, device_img_dir)
             shutil.copy2(csvlName, device_csv_dir)
-    '''
+
+
+
+
 
     # 查看csv中的异常经纬度
     device_csv_dir = r'E:/test_opencv/轨迹分析/device_csv/'
     device_image_dir = r'E:/test_opencv/轨迹分析/device_image/'
-    normal_device_img_dir = r'E:/test_opencv/轨迹分析/normal_device_image'
-    normal_device_csv_dir = r'E:/test_opencv/轨迹分析/normal_device_csv'
-    abnormal_device_img_dir = r'E:/test_opencv/轨迹分析/abnormal_device_image'
-    abnormal_device_csv_dir = r'E:/test_opencv/轨迹分析/abnormal_device_csv'
+    normal_device_img_dir = r'E:/test_opencv/轨迹分析/normal_device_image/'
+    normal_device_csv_dir = r'E:/test_opencv/轨迹分析/normal_device_csv/'
+    abnormal_device_img_dir = r'E:/test_opencv/轨迹分析/abnormal_device_image/'
+    abnormal_device_csv_dir = r'E:/test_opencv/轨迹分析/abnormal_device_csv/'
+    normal_dbscan_device_img_dir = r'E:/test_opencv/轨迹分析/normal_dbscan_device_image/'
+    abnormal_dbscan_device_img_dir = r'E:/test_opencv/轨迹分析/abnormal_dbscan_device_image/'
     normal_device_list = []
     abnormal_device_list = []
+
+    #经纬度差1度的数据分文件夹存放
     for name in os.listdir(device_csv_dir):
-        csv_name = device_csv_dir + os.sep + name
+        csv_name = device_csv_dir + name
         print(csv_name)
-        names = ['device_id', 'upload_time', 'latitude', 'longitude', 'mileage', 'other_vals', 'speed', 'upload_time_1']
         df = pd.read_csv(csv_name, encoding='utf-8', parse_dates=[1], low_memory=False)
-        X = df[['latitude', 'longitude']]
-        X = X.drop_duplicates()
-        # X = df.iloc[:,2:4]
-        # convert eps to radians for use by haversine
-        kms_per_rad = 6371.0088
-        epsilon = 1.5 / kms_per_rad
-        # Extract intersection coordinates (latitude, longitude)
-        dbsc = (DBSCAN(eps=epsilon, min_samples=1, algorithm='ball_tree', metric='haversine').fit(np.radians(X)))
-        # dbsc = (DBSCAN(eps=epsilon, min_samples=1,n_jobs=1).fit(np.radians(X)))
-        fac_cluster_labels = dbsc.labels_
-        # get the number of clusters
-        num_clusters = len(set(dbsc.labels_))
-        # turn the clusters into a pandas series,where each element is a cluster of points
-        dbsc_clusters = pd.Series([X[fac_cluster_labels == n] for n in range(num_clusters)])
-        # get centroid of each cluster
-        fac_centroids = dbsc_clusters.map(get_centroid)
-        # unzip the list of centroid points (lat, lon) tuples into separate lat and lon lists
-        cent_lats, cent_lons = zip(*fac_centroids)
-        # from these lats/lons create a new df of one representative point for eac cluster
-        centroids_pd = pd.DataFrame({'longitude': cent_lons, 'latitude': cent_lats})
-        # Plot the faciity clusters and cluster centroid
-        fig, ax = plt.subplots(figsize=[20, 10])
-        facility_scatter = ax.scatter(X['longitude'], X['latitude'], c=fac_cluster_labels,
-                                       edgecolor='None', alpha=0.7, s=120)
-        centroid_scatter = ax.scatter(centroids_pd['longitude'], centroids_pd['latitude'], marker='x', linewidths=2,
-                                      c='k', s=50)
-        ax.set_title('Facility Clusters & Facility Centroid', fontsize=30)
-        ax.set_xlabel('Longitude', fontsize=24)
-        ax.set_ylabel('Latitude', fontsize=24)
-        ax.legend([facility_scatter, centroid_scatter], ['Facilities', 'Facility Cluster Centroid'], loc='upper right',
-                  fontsize=20)
-        # plt.show()
-        plt.savefig(r'E:/test_opencv/轨迹分析/device_DBSCAN_image/' + name.split('.')[0] + '.png')
-        plt.close()
+        m1 = df[['latitude', 'longitude']].diff().abs().gt(0.1)
+        m2 = df[['latitude', 'longitude']].shift().diff().abs().gt(0.1)
+        m = m1 | m2
+        latitude_diff_list = df.index[m['latitude']].tolist()
+        longitude_diff_list = df.index[m['longitude']].tolist()
+        if not latitude_diff_list and not longitude_diff_list: #如果经纬度的list为空，说明是正常数据
+            normal_device_list.append(name.split('.')[0])
+        else:
+            abnormal_device_list.append(name.split('.')[0])
+
+    for item in normal_device_list:
+        imageName = device_image_dir + str(item) + '.png'
+        csvlName = device_csv_dir + str(item) + '.csv'
+        if os.path.isfile(imageName) and os.path.isfile(csvlName):
+            shutil.copy2(imageName, normal_device_img_dir)
+            shutil.copy2(csvlName, normal_device_csv_dir)
+
+    for item in abnormal_device_list:
+        imageName = device_image_dir + str(item) + '.png'
+        csvlName = device_csv_dir + str(item) + '.csv'
+        if os.path.isfile(imageName) and os.path.isfile(csvlName):
+            shutil.copy2(imageName, abnormal_device_img_dir)
+            shutil.copy2(csvlName, abnormal_device_csv_dir)
+
+    #正常的数据聚类
+    for item in os.listdir(normal_device_csv_dir):
+        csvlName = normal_device_csv_dir + item
+        draw_with_dbscan(csvlName, item, normal_dbscan_device_img_dir)
+
+    #不正常的数据聚类
+    for item in os.listdir(abnormal_device_csv_dir):
+        csvlName = abnormal_device_csv_dir + item
+        draw_with_dbscan(csvlName, item, abnormal_dbscan_device_img_dir)
 
 
-        # m1 = df[['latitude', 'longitude']].diff().abs().gt(0.1) #
-        # m2 = df[['latitude', 'longitude']].shift().diff().abs().gt(0.1)
-        # m = m1 | m2
-        # latitude_diff_list = df.index[m['latitude']].tolist()
-        # longitude_diff_list = df.index[m['longitude']].tolist()
-        # if not latitude_diff_list and not longitude_diff_list: #如果经纬度的list为空，说明是正常数据
-        #     normal_device_list.append(name.split('.')[0])
-        # else:
-        #     abnormal_device_list.append(name.split('.')[0])
 
-    # for item in normal_device_list:
-    #     imageName = device_image_dir + str(item) + '.png'
-    #     csvlName = device_csv_dir + str(item) + '.csv'
-    #     if os.path.isfile(imageName) and os.path.isfile(csvlName):
-    #         shutil.copy2(imageName, normal_device_img_dir)
-    #         shutil.copy2(csvlName, normal_device_csv_dir)
-
-    # for item in abnormal_device_list:
-    #     imageName = device_image_dir + str(item) + '.png'
-    #     csvlName = device_csv_dir + str(item) + '.csv'
-    #     if os.path.isfile(imageName) and os.path.isfile(csvlName):
-    #         shutil.copy2(imageName, abnormal_device_img_dir)
-    #         shutil.copy2(csvlName, abnormal_device_csv_dir)
+def draw_with_dbscan(para_csv_path_name,para_csv_name,para_save_path):
+    names = ['device_id', 'upload_time', 'latitude', 'longitude', 'mileage', 'other_vals', 'speed', 'upload_time_1']
+    df = pd.read_csv(para_csv_path_name, encoding='utf-8', parse_dates=[1], low_memory=False)
+    X = df[['latitude', 'longitude']]
+    X = X.drop_duplicates()
+    # X = df.iloc[:,2:4]
+    # convert eps to radians for use by haversine
+    kms_per_rad = 6371.0088  # mean radius of the earth
+    epsilon = 1.5 / kms_per_rad  # The maximum distance between two samples for one to be considered as in the neighborhood of the other. This is not a maximum bound on the distances of points within a cluster. This is the most important DBSCAN parameter to choose appropriately for your data set and distance function. default=0.5
+    dbsc = (DBSCAN(eps=epsilon, min_samples=1, algorithm='ball_tree', metric='haversine').fit(np.radians(X)))
+    # dbsc = (DBSCAN(eps=epsilon, min_samples=1,n_jobs=1).fit(np.radians(X)))
+    fac_cluster_labels = dbsc.labels_
+    # get the number of clusters
+    num_clusters = len(set(dbsc.labels_))
+    # turn the clusters into a pandas series,where each element is a cluster of points
+    dbsc_clusters = pd.Series([X[fac_cluster_labels == n] for n in range(num_clusters)])
+    # get centroid of each cluster
+    fac_centroids = dbsc_clusters.map(get_centroid)
+    # unzip the list of centroid points (lat, lon) tuples into separate lat and lon lists
+    cent_lats, cent_lons = zip(*fac_centroids)
+    # from these lats/lons create a new df of one representative point for eac cluster
+    centroids_pd = pd.DataFrame({'longitude': cent_lons, 'latitude': cent_lats})
+    # Plot the faciity clusters and cluster centroid
+    fig, ax = plt.subplots(figsize=[20, 10])
+    facility_scatter = ax.scatter(X['longitude'], X['latitude'], c=fac_cluster_labels,
+                                  edgecolor='None', alpha=0.7, s=120)
+    centroid_scatter = ax.scatter(centroids_pd['longitude'], centroids_pd['latitude'], marker='x', linewidths=2,
+                                  c='k', s=50)
+    ax.set_title('Facility Clusters & Facility Centroid', fontsize=30)
+    ax.set_xlabel('Longitude', fontsize=24)
+    ax.set_ylabel('Latitude', fontsize=24)
+    ax.legend([facility_scatter, centroid_scatter], ['Facilities', 'Facility Cluster Centroid'], loc='upper right',
+              fontsize=20)
+    # plt.show()
+    plt.savefig(para_save_path + para_csv_name.split('.')[0] + '.png')
+    plt.close()
 
 
 def get_centroid(cluster):
